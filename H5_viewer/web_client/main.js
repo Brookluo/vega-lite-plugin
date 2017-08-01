f3ee24275fe742d833d076e2e9572cef7e857034
Yufeng Luo <brook@Yufengs-MacBook-Pro.local>
2017-07-31 21:09:55 -0500

    Add two templates in webclient side

--- H5_viewer/web_client/main.js
+++ H5_viewer/web_client/main.js
@@ -1,9 +1,13 @@
 import { wrap } from 'girder/utilities/PluginUtils';
 import ItemView from 'girder/views/body/ItemView';
+import $ from 'jquery';
+import _ from 'underscore'
+import
 
 wrap(ItemView, 'render', function (render) {
   this.once('g:rendered', function() {
     this.$('.g-item-header').after('<div class = "g-H5-view"></div>');
-    
+
+
   }
 }
